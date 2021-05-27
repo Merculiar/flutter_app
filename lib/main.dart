f41922e6fd62d343ea39c4c9b09637533d7ccd20
Merculiar <vladsum00@gmail.com>
2021-05-27 15:18:19 +0300

    Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,13 @@ import 'package:flutter/widgets.dart';
 import 'dart:math';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/':(BuildContext context) => MyApp(),
-      '/second':(BuildContext context) => SecondPage()
-    }
-  ));
+  runApp(MaterialApp(initialRoute: '/', routes: {
+    '/': (BuildContext context) => MyApp(),
+    '/second': (BuildContext context) => SecondPage()
+  }));
 }
 
 class MyApp extends StatefulWidget {
-
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -62,42 +58,46 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Друге вікно'),backgroundColor: Colors.red,),
-      body: Container(
-        color: Colors.pink[100],
-        alignment: Alignment.bottomCenter,
-        child: Column(
+        appBar: AppBar(
+          title: Text('Друге вікно'),
+          backgroundColor: Colors.red,
+        ),
+        body: Container(
+          color: Colors.pink[100],
+          alignment: Alignment.bottomCenter,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               //photo
               Image.asset('assets/images/KeepCalm.jpg'),
               //text1
               Text('Keep calm!',
-                textDirection: TextDirection.ltr,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      )),
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  )),
               //text2
               Text('You can go back if you press the button below',
-                textDirection: TextDirection.ltr,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      )),  
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  )),
               //button
               ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.red, 
-                  onPrimary: Colors.white, 
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    onPrimary: Colors.white,
                   ),
-                onPressed: () {Navigator.pop(context);},
-                child: Text('Go Back')) 
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Go Back'))
             ],
-        ),
-      )
-    );
+          ),
+        ));
   }
 }
